0e62305214a7923cceca70822a9af0bce77ddcbd
Ibrahim Saleh <ibr.sale7@gmail.com>
2024-04-28 11:44:18 +0300

    remove debugger

--- src/services/pokemonAPI.tsx
+++ src/services/pokemonAPI.tsx
@@ -1,12 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const pokemonApi = createApi({
+  reducerPath: 'pokemonApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
-  endpoints: (builder) => {debugger; return ({
+  tagTypes: [],
+  endpoints: (builder) => ({
     getPokemonByName: builder.query({
       query: (name: string) => `pokemon/${name}`,
     }),
-  })},
+  }),
 })
 
 // Export hooks for usage in functional components
